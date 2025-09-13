57529f05e15d406f3ea968fadae580d2ee770bb2
PAVAN864 <mspavankumar864@gmail.com>
2025-09-13 19:55:43 +0530

    Added Portfolio Code

---
+++ src/config.ts
@@ -0,0 +1,61 @@
+export const siteConfig = {
+  name: "Karthik R T",
+  title: "CyberSecurity Enthusiast",
+  description: "Portfolio website of Karthik",
+  accentColor: "#1d4ed8",
+  social: {
+    email: " karthikrt2003@gmail.com",
+    linkedin: "https://www.linkedin.com/in/karthik-r-t/",
+    github: "https://github.com/recursive-bot",
+  },
+  aboutMe:"CyberSecurity enthusiast with hands-on experience in penetration testing, exploit development, and red-teaming across web, network, and cloud environments. Skilled in uncovering and weaponizing vulnerabilities using tools like Burp Suite, Metasploit, Nmap, and SQLmap, with scripting expertise in Python and Bash. CEH-certified with proven success in CTFs, VAPT projects, and SOC operations, aligning work to OWASP Top 10 and MITRE ATT&CK frameworks.",
+  skills: [
+  "Network Security",
+  "Penetration Testing",
+  "Incident Response",
+  "Security Information and Event Management (SIEM)",
+  "Cryptography",
+  "Vulnerability Assessment",
+  "Malware Analysis",
+  "Cloud Security",
+  "Ethical Hacking",
+  "Data Loss Prevention (DLP)",
+],
+  projects: [
+    {
+      name: "Pwnagotchi",
+      description:"A gamified wireless security auditing tool, to identify and assess vulnerabilities in Wi-Fi networks",
+      link: "https://aidevroundup.com/?ref=devportfolio",
+      skills: [ "WiFi Handshakes", "Encryption Protocols"],
+    },
+    {
+      name: "SOC Operations with IBM QRadar and Splunk",
+      description:"Implemented and configured IBM QRadar and Splunk SIEM platforms to monitor, analyze, and respond to security events across enterprise systems. Focused on log ingestion, correlation rules, and alert generation to enhance threat detection and incident response capabilities.",
+      link: "https://fullstackextensions.com/?ref=devportfolio",
+      skills: ["SIEM Configuration & Management", "Log Analysis & Threat Detection", "Incident Response Automation","Security Event Correlation"],
+    }
+  ],
+  experience: [
+    {
+      company: "Cartel Software Pvt Ltd",
+      title: "Cybersecurity Professional Intern",
+      dateRange: "Jan 2025 - Jun 2025",
+      bullets: [
+        "Conducted 20+ penetration tests on enterprise web apps and networks using Burp Suite, OWASP ZAP, and manual exploitation, uncovering critical OWASP Top 10 flaws.",
+        "Applied Metasploit, custom exploits, and PoCs to validate vulnerabilities, demonstrating real-world attack paths and strengthening security posture",
+        "Performed SIEM-based investigations in IBM QRadar and Splunk; tuned detection rules to reduce false positives, improving SOC triage efficiency",
+        "Delivered executive-level and technical VAPT reports mapped to MITRE ATT&CK, documenting CVSS scores, risk ratings, and validated remediation steps."
+      ],
+    }
+  ],
+  education: [
+    {
+      school: "PES University",
+      degree: "Bachelor of Technology in Eletronics and Communication",
+      dateRange: "2022 - 2025",
+      achievements: [
+        "Graduated PES University with 6.71 GPA",
+      ],
+    },
+  ],
+};
