94168d79f36433c9e43f7dbd68763cc87775627e
PAVAN864 <mspavankumar864@gmail.com>
2025-09-13 21:18:53 +0530

    Dist updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/recursive-bot/devportfolio-master/',
+  base: '/devportfolio-master/',
   vite: {
     plugins: [tailwindcss()],
   },
