593dba34f6a3dafb22dfd2c7ca5b26b7e7ffc5dc
PAVAN864 <mspavankumar864@gmail.com>
2025-09-13 21:12:45 +0530

    Added base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/recursive-bot/',
+  base: '/recursive-bot/devportfolio-master/',
   vite: {
     plugins: [tailwindcss()],
   },
